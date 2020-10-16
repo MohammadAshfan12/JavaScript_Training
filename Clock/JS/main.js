0399f9476873980b1285a8fdb58bb1014a8539d4
Mohammad Ashfan <mohammadashfan12>
2020-10-16 10:26:26 +0530

    updates code

--- Clock/JS/main.js
+++ Clock/JS/main.js
@@ -1,8 +1,7 @@
 var hourHand = document.getElementById('hr');
 var minuteHand = document.getElementById('min')
 var secondsHand = document.getElementById('sec')
-var arr = [];
-var arr2 = [];
+var minuteArray = [];
 
 function setClock(wait){
     var date = new Date();
@@ -10,24 +9,20 @@ function setClock(wait){
     var minute = date.getMinutes()
     var seconds = date.getSeconds();
     var hourDeg = (hour * 30) + (0.5 * minute);
-    
     var minDeg = (minute * 6) + (0.1 * seconds)
     var secDeg = seconds * 6;
     setTimeout(second,wait);
- 
-    function second(){
-        if(minute === 0){
-            hourHand.style.transform = 'rotate(' + hourDeg + 'deg)';
-        }
 
-        if(arr.includes(minute)){
+    function second(){
+        if(minuteArray.includes(minute)){
             secondsHand.style.transform = 'rotate(' + secDeg + 'deg)';
         }
         else{
             secondsHand.style.transform = 'rotate(' + secDeg + 'deg)';
             minuteHand.style.transform = 'rotate(' + minDeg + 'deg)';
-            arr = []
-            arr.push(minute);
+            hourHand.style.transform = 'rotate(' + hourDeg + 'deg)';
+            minuteArray = []
+            minuteArray.push(minute);
         }  
         setClock(wait)
     } 
