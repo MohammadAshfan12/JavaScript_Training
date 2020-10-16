9345e573169a85a6331cf7d6356208cd340511b4
Mohammad Ashfan <mohammadashfan12>
2020-10-16 10:09:37 +0530

    updated

--- Clock/JS/main.js
+++ Clock/JS/main.js
@@ -1,24 +1,39 @@
-console.log('h')
 var hourHand = document.getElementById('hr');
 var minuteHand = document.getElementById('min')
 var secondsHand = document.getElementById('sec')
+var arr = [];
+var arr2 = [];
 
-function setClock() {
-    
+function setClock(wait){
     var date = new Date();
     var hour = date.getHours() % 12;
-    var minute = date.getMinutes() 
-    console.log(minute)
+    var minute = date.getMinutes()
     var seconds = date.getSeconds();
-
     var hourDeg = (hour * 30) + (0.5 * minute);
-    var minDeg = (minute * 6) + (0.1 * seconds);
+    
+    var minDeg = (minute * 6) + (0.1 * seconds)
     var secDeg = seconds * 6;
+    setTimeout(second,wait);
+ 
+    function second(){
+        if(minute === 0){
+            hourHand.style.transform = 'rotate(' + hourDeg + 'deg)';
+        }
 
-    hourHand.style.transform = 'rotate('+ hourDeg + 'deg)';
-    minuteHand.style.transform = 'rotate('+ minDeg + 'deg)';
-    secondsHand.style.transform = 'rotate('+ secDeg + 'deg)';
-    setTimeout(setClock, 1000);
-};
+        if(arr.includes(minute)){
+            secondsHand.style.transform = 'rotate(' + secDeg + 'deg)';
+        }
+        else{
+            secondsHand.style.transform = 'rotate(' + secDeg + 'deg)';
+            minuteHand.style.transform = 'rotate(' + minDeg + 'deg)';
+            arr = []
+            arr.push(minute);
+        }  
+        setClock(wait)
+    } 
+}
 
-setClock();
+setClock(1000);
+
+
+
