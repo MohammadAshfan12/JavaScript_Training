864779b07a4f1c1b802a19c26de3774d63acf238
Mohammad Ashfan <mohammadashfan12>
2020-10-16 21:53:37 +0530

    updated clock

--- Clock/JS/main.js
+++ Clock/JS/main.js
@@ -1,31 +1,62 @@
 var hourHand = document.getElementById('hr');
 var minuteHand = document.getElementById('min')
 var secondsHand = document.getElementById('sec')
-var minuteArray = [];
+var arr = [];
+var arr2 = [];
+var flag = false;
+var hour;
+var minute;
+var seconds;
 
-function setClock(wait){
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function changeTime() {
+    flag = true;
+    hour = document.getElementById('hor').value;
+    minute = document.getElementById('mon').value;
+    seconds = 0;
+}
+
+function setClock(wait) {
     var date = new Date();
-    var hour = date.getHours() % 12;
-    var minute = date.getMinutes()
-    var seconds = date.getSeconds();
+    if (flag === false) {
+        hour = date.getHours() % 12;
+        minute = date.getMinutes()
+    }
+    seconds = date.getSeconds();
+    if ((seconds === 0) && (flag === true)) {
+        minute++;
+    }
     var hourDeg = (hour * 30) + (0.5 * minute);
     var minDeg = (minute * 6) + (0.1 * seconds)
     var secDeg = seconds * 6;
-    setTimeout(second,wait);
+    
+    setTimeout(second, wait);
 
-    function second(){
-        if(minuteArray.includes(minute)){
+    function second() {
+        if (minute === 60) {
+            minute = 0;
+            hour++;
+        }
+        if (arr.includes(minute) && flag === false) {
             secondsHand.style.transform = 'rotate(' + secDeg + 'deg)';
         }
-        else{
+        else {
             secondsHand.style.transform = 'rotate(' + secDeg + 'deg)';
+            if(flag === false || seconds === 0){
             minuteHand.style.transform = 'rotate(' + minDeg + 'deg)';
             hourHand.style.transform = 'rotate(' + hourDeg + 'deg)';
-            minuteArray = []
-            minuteArray.push(minute);
-        }  
+            }
+            arr = []
+            arr.push(minute);
+        }
+
+
         setClock(wait)
-    } 
+    }
+
 }
 
 setClock(1000);
