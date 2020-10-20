6a5ae9b74f2bbb90caae2daeb987f52012933d57
Mohammad Ashfan <mohammadashfan12>
2020-10-20 11:09:02 +0530

    updated code

---
+++ Analog-clock/clockLib.js
@@ -0,0 +1,61 @@
+const hourHand = document.getElementById('hour-hand')
+const minuteHand = document.getElementById('minute-hand')
+const secondHand = document.getElementById('second-hand')
+var hour1, minute1, myfunc;
+
+var flag = true;
+export class clock {
+    constructor(currentDate, h1, m1, flag) {
+        if (flag === true) {
+            this.hour = currentDate.getHours();
+            this.minute = currentDate.getMinutes();
+            this.second = currentDate.getSeconds();
+            console.log(this.hour, this.minute, this.second)
+        }
+        else {
+            this.hour = Number(h1);
+            this.minute = Number(m1);
+            this.second = 1;
+        }
+    }
+    display() {
+        if(flag === true){
+        myfunc = setInterval(() => {
+            const secondsRatio = this.second++ / 60
+            const minutesRatio = (secondsRatio + this.minute) / 60
+            const hoursRatio = (minutesRatio + this.hour) / 12
+            this.setRotation(secondHand, secondsRatio)
+            this.setRotation(minuteHand, minutesRatio)
+            this.setRotation(hourHand, hoursRatio);
+        }, 1000)
+    }
+    else{
+        clearInterval(myfunc);
+        setInterval(() => {
+            const secondsRatio = this.second++ / 60
+            const minutesRatio = (secondsRatio + this.minute) / 60
+            const hoursRatio = (minutesRatio + this.hour) / 12
+            this.setRotation(secondHand, secondsRatio)
+            this.setRotation(minuteHand, minutesRatio)
+            this.setRotation(hourHand, hoursRatio);
+        }, 1000)
+    }
+    }
+    setRotation(hand, rotationRatio) {
+        hand.style.setProperty('--rotation', rotationRatio * 360)
+    }
+}
+const a = document.getElementById('btn').onclick = changeTime;
+const currentDate = new Date();
+function changeTime() {
+    flag = false;
+    hour1 = document.getElementById('hor').value
+    minute1 = document.getElementById('mon').value
+    let obj3 = new clock(null, hour1, minute1, flag)
+    obj3.display();
+}
+if(flag === true){
+    let obj = new clock(currentDate, hour1, minute1, flag);
+    obj.display();
+}
+
